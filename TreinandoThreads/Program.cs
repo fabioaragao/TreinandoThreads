3f7233ac7df7e097aa2376828a4f8f8c60e3e74b
EuEMinhaCasa <EuEMinhaCasa@DESKTOP-0MKUQUE>
2018-11-02 16:34:54 -0300

    Aprendendo Threads

--- TreinandoThreads/Program.cs
+++ TreinandoThreads/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace TreinandoThreads
 {
     class Program
     {
+        static void escreveralgo() {
+            Thread.Sleep(5000);
+            Console.WriteLine("Inicia thread após 5 secs...");
+        }
         static void Main(string[] args)
         {
+            Thread t = new Thread(escreveralgo);
+            t.Start();
+
+            Console.WriteLine("Apareci..");
+
+            Console.ReadKey();
         }
     }
 }
